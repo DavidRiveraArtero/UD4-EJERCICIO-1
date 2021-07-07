13610ce071178a9698c5d69cb122088ba02d334b
DavidRiveraArtero <davidtoy222@gmail.com>
2021-07-07 10:29:27 +0200

    EJ1-UD4

---
+++ UD4_OperacionesEj1App.java
@@ -0,0 +1,26 @@
+//OPERACIONES SIMPLES 
+
+//DAVID RIVERA ARTERO
+
+
+public class UD4_OperacionesEj1App {
+
+	public static void main(String[] args) {
+		int a=20;
+		
+		int b=30;
+		
+		//PRINTAMOS LA SUMA
+		System.out.println(a + b);
+		//PRINTAMOS LA RESTA
+		System.out.println(a - b);
+		//PRINTAMOS LA MULTIPLICACION 
+		System.out.println(a * b);
+		//PRINTAMOS LA DIVISION
+		System.out.println(a / b);
+		//PRINTAMOS EL MODULO
+		System.out.println(a % b);
+
+	}
+
+}
